9a25d86f3519c51cee63299feb75279b9ad45a21
ethornbury <ethornbury@eircom.net>
2015-07-31 08:53:20 +0000

    test files and database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,49 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#the code below is used with the gem'faker' to create sample users
-#require 'faker'
-Employee.create!(firstname:  "Emer",
-             lastname: "Admin",
-             email: "example@et.ie",
+
+require 'faker'
+Employee.create!(firstname:  "Admin",
+             lastname: "User",
+             email: "admin@email.com",
              admin: true,
              jobType: "Admin")
              
 Employee.create!(firstname:  "John",
              lastname: "Doe",
-             email: "example1@et.ie",
+             email: "example1@email.com",
              jobType: "Sales")
 
 Employee.create!(firstname:  "Mary",
              lastname: "Cullen",
-             email: "example2@et.ie",
+             email: "example2@email.com",
              jobType: "Sales")
              
 Employee.create!(firstname:  "Mark",
              lastname: "Smith",
-             email: "example3@et.ie",
+             email: "example3@email.com",
              jobType: "Sales")
 
 Employee.create!(firstname:  "Anne",
              lastname: "McCarthy",
-             email: "example4@et.ie",
+             email: "example4@email.com",
              jobType: "Sales")
              
-User.create!(firstname:  "Tim",
-             lastname: "Burn",
-             email: "example5@et.ie",
-             jobType: "Sales")
+User.create!(email: "admin@email.com",
+             admin: true,
+             password:              password,
+             password_confirmation: password)
 
-50.times do |n|
-  firstname  = Faker::Name.name
-  lastname   = Faker::Name.name
-  email = "example-#{n+1}@et.ie"
-  Employee.create!(name: firstname,
-    name: lastname,
-    email: email)
-end
+#50.times do |n|
+#  firstname  = Faker::Name.name
+#  lastname   = Faker::Name.name
+#  email = "example-#{n+1}@email.com"
+#  Employee.create!(name: firstname,
+#    name: lastname,
+#    email: email)
+#end
 
-10.times do |n|
-  email = "example-#{n+1}@et.ie"
+5.times do |n|
+  email = "example-#{n+1}@email.com"
   password = "password"
   User.create!(email: email,
                password:              password,
--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  should validate_presence_of(:firstname).with_message(/can't be empty/)
-  should validate_presence_of(:lastname).with_message(/can't be empty/) 
-  should validate_presence_of(:email).with_message(/can't be empty/)
+  should validate_presence_of(:firstname).with_message(/can't be blank/)
+  should validate_presence_of(:lastname).with_message(/can't be blank/) 
+  should validate_presence_of(:email).with_message(/can't be blank/)
  
   should_not allow_value("blah").for(:email)
 end
